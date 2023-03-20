5cd33d5282dcc2efce388830a05da9a970f8504e
Alireza Tabatabaeian <ali.tabatabaeian16@gmail.com>
2023-03-20 18:27:29 +0330

    add absolute url and remove content representation in task list

--- core/todo/api/v1/serializers.py
+++ core/todo/api/v1/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework import serializers
 from todo.models import Task
+from accounts.models import Profile
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    absolute_url = serializers.SerializerMethodField(method_name='get_absolute_url')
     class Meta:
         model = Task
-        fields = ['user', 'content', 'created_date', 'updated_date', 'isDone']
+        fields = ['id', 'user', 'content', 'created_date', 'updated_date', 'isDone', 'absolute_url']
+
+    def get_absolute_url(self, obj=None):
+        request = self.context.get('request')
+        url = request.build_absolute_uri()
+        if 'task/' in url:
+            return url
+        return request.build_absolute_uri(obj.pk).replace('tasks', 'task')+'/'
+
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        rep = super().to_representation(instance)
+        rep['user'] = Profile.objects.get(pk=rep['user']).user.email
+        if not request.parser_context.get('kwargs').get('pk', False):
+            rep.pop('content', None)
+        return rep
--- core/todo/api/v1/urls.py
+++ core/todo/api/v1/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from todo.api.v1 import views
 app_name = 'todo-api'
 urlpatterns = [
-    path('tasks', views.TaskList.as_view({'get': 'list', 'post': 'create'}), name='task-list')
+    path('tasks/', views.TaskList.as_view({'get': 'list', 'post': 'create'}), name='task-list'),
+    path('task/<int:pk>/', views.TaskList.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update',
+                                                   'delete': 'destroy'}), name='task-detail'),
 ]
