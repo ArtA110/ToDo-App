efcdf7c357d65415cb6b12ee28d7f3b02b8fa997
Alireza Tabatabaeian <ali.tabatabaeian16@gmail.com>
2023-03-21 15:51:33 +0330

    Permissions added for user API

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -1,11 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 from accounts.models import User, Profile
 from .serializers import UserSerializer
+from .permissions import IsOwnerOrAdmin
 
 
 class UserView(ModelViewSet):
     queryset = User.object.filter(is_active=True)
     serializer_class = UserSerializer
+    permission_classes = [IsOwnerOrAdmin]
 
 # class ProfileView(ModelViewSet):
 #     pass
