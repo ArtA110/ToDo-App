11dd9bb9ddfa701ae3571a32dd162aa3412e33f5
Alireza Tabatabaeian <ali.tabatabaeian16@gmail.com>
2023-03-21 06:09:38 +0330

    Ordering added for Task API

--- core/todo/api/v1/views.py
+++ core/todo/api/v1/views.py
@@ -5,12 +5,14 @@ from .permissions import IsOwnerOrAdmin
 from .filters import TaskFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .paginations import DefaultPagination
+from rest_framework import filters
 
 
 class TaskView(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsOwnerOrAdmin]
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filterset_class = TaskFilter
     pagination_class = DefaultPagination
+    ordering_fields = ['created_date', 'published_date']
