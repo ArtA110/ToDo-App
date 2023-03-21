f80662d9ff310e296e176fd5b896e6fb3701b8ef
Alireza Tabatabaeian <ali.tabatabaeian16@gmail.com>
2023-03-21 05:56:10 +0330

    Add more useful filter for task API

--- core/todo/api/v1/filters.py
+++ core/todo/api/v1/filters.py
@@ -1,13 +1,21 @@
-import django_filters
+import django_filters.rest_framework as filters
 from todo.models import Task
+from django.forms import CheckboxInput, DateTimeInput
+from accounts.models import Profile
 
 
-class TaskFilter(django_filters.FilterSet):
-    created_date_gt = django_filters.DateTimeFilter(field_name='created_date', lookup_expr='gt')
-    created_date_lt = django_filters.DateTimeFilter(field_name='created_date', lookup_expr='lt')
-    updated_date_gt = django_filters.DateTimeFilter(field_name='updated_date', lookup_expr='gt')
-    updated_date_lt = django_filters.DateTimeFilter(field_name='updated_date', lookup_expr='lt')
+class TaskFilter(filters.FilterSet):
+    created_date_gt = filters.DateTimeFilter(field_name='created_date', lookup_expr='gt',
+                                            widget=DateTimeInput(attrs={'type': 'datetime-local'}))
+    created_date_lt = filters.DateTimeFilter(field_name='created_date', lookup_expr='lt',
+                                            widget=DateTimeInput(attrs={'type': 'datetime-local'}))
+    updated_date_gt = filters.DateTimeFilter(field_name='updated_date', lookup_expr='gt',
+                                            widget=DateTimeInput(attrs={'type': 'datetime-local'}))
+    updated_date_lt = filters.DateTimeFilter(field_name='updated_date', lookup_expr='lt',
+                                            widget=DateTimeInput(attrs={'type': 'datetime-local'}))
+    isDone = filters.BooleanFilter(field_name='isDone', widget=CheckboxInput())
+    user = filters.ModelMultipleChoiceFilter(field_name='user', queryset=Profile.objects.filter(user__is_active=True))
 
     class Meta:
         model = Task
-        fields = ['created_date', 'updated_date', 'isDone']
+        fields = ['created_date_gt', 'created_date_lt', 'updated_date_gt', 'updated_date_lt', 'isDone', 'user']
